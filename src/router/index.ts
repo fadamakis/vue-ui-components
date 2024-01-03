e8938a168c39a8da02c4cca440676cef3e61d066
Fotis Adamakis <fadamakis@gmail.com>
2024-01-03 15:13:24 +0100

    Update router config

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import InputDemoPage from '../views/InputDemoPage.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/vue-ui-components/'),
   routes: [
     {
       path: '/AppInput',
